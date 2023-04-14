ba6388d5f875f1217e9d8663dc1de79ed6f15dc2
ThienNTN <nguyentrannhatthien1710@gmail.com>
2023-04-14 14:13:12 +0700

    config new credential

--- set_env.sh
+++ set_env.sh
@@ -1,11 +1,10 @@
 # This file is used for convenience of local development.   
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=thienntn1
-export POSTGRES_PASSWORD=thienntn1
-export POSTGRES_HOST=project3-database.cyjxqe59uk6w.us-east-2.rds.amazonaws.com
-export POSTGRES_DB=postgres
-export AWS_BUCKET=project3-cloud-developer2
-export AWS_REGION=us-east-2
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=postgres
+export POSTGRES_HOST=postgres.cgsj0ur26vo1.us-east-1.rds.amazonaws.com
+export AWS_BUCKET=cloud-developer-s3
+export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=hello
 export URL=http://localhost:8100
